21eaa725b718e16708a0c867235beafe4f2f321b
zhouminsen <992427>
2018-07-19 20:55:32 +0800

    删除一些不要的配置

---
+++ src/main/java/com/zjw/jdk/thread/zk/ThreadTest.java
@@ -0,0 +1,39 @@
+package com.zjw.jdk.thread.zk;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by zhoum on 2018/5/16.
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(() -> {
+                System.out.println(bb());
+            });
+            executorService.submit(thread);
+        }
+        executorService.shutdown();
+        System.out.println("111我是主线程");
+
+
+    }
+
+    private static int bb() {
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(() -> {
+                System.out.println(Thread.currentThread().getName());
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
+        }
+        System.out.println("我是主线程");
+        return 1;
+    }
+}
