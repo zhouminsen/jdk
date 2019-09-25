99bf1cf3aa1d015e2282d820da1bf3b92969d41d
110562 <weijia1992427>
2019-09-25 13:18:22 +0800

    xml、实体互转

---
+++ src/main/java/com/zjw/jdk/xml/User.java
@@ -0,0 +1,60 @@
+package com.zjw.jdk.xml;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Steven
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+// XML文件中的根标识
+@XmlRootElement(name = "User")
+// 控制JAXB 绑定类中属性和字段的排序
+@XmlType(propOrder = {
+        "userId",
+        "userName",
+        "password",
+        "birthday",
+        "money",
+        "computers"
+})
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // 用户Id
+    private int userId;
+    // 用户名
+    private String userName;
+    // 用户密码
+    private String password;
+    // 用户生日
+    private Date birthday;
+    // 用户钱包
+    private double money;
+    // 拥有的电脑
+    private List<Computer> computers;
+
+    public User() {
+        super();
+    }
+
+    public User(int userId, String userName, String password, Date birthday,
+                double money) {
+        super();
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.birthday = birthday;
+        this.money = money;
+    }
+
+
+}
